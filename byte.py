8fc46d8899349afa6366a5e1dcb450c2262215ca
Ori <45724082+therealOri@users.noreply.github.com>
2021-11-27 19:44:56 -0700

    Update byte.py

--- byte.py
+++ byte.py
@@ -5,19 +5,19 @@ from blib import bl
 #------------Code Start.------------#
 bl.clear()
 try:
-    a = int(input(f"{bl.banner()}\n\nWhat would you like to do?\n\n1. Encode\n2. Decode\n3. Get hash\n4. Compare hashes\n\nEnter: "))
+    option = int(input(f"{bl.banner()}\n\nWhat would you like to do?\n\n1. Encode\n2. Decode\n3. Get hash\n4. Compare hashes\n\nEnter: "))
 except Exception as e:
     bl.clear()
     print(f'Value given is not an integer.\nError: {e}')
     quit()
     
-if (a == 1):
+if (option == 1):
     bl.encode()
-elif (a == 2):
+elif (option == 2):
     print(f"Decoded Data:  {bl.decode()}")
-elif (a == 3):
+elif (option == 3):
     bl.check()
-elif (a == 4):
+elif (option == 4):
     bl.clear()
     fhash = input('Please provide a sha256 hash to compare: ')
     bl.clear()
