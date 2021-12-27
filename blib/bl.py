6a235ed8f2f7e1f4bd02f4173c26a06bde32ae6e
Ori <45724082+therealOri@users.noreply.github.com>
2021-12-27 13:26:30 -0700

    Update bl.py

--- blib/bl.py
+++ blib/bl.py
@@ -3,6 +3,14 @@ import os
 from os.path import exists as file_exists
 import hashlib
 
+import time
+import cv2
+import numpy as np
+import math
+import shutil
+from stegano import lsb
+from subprocess import call, STDOUT
+
 
 #------------My functions------------#
 
@@ -86,6 +94,75 @@ def compare(fhash):
         print(f'The file with the name "{file}" does not exist in the current directory.')
         quit()
 # End Compare hash
+
+
+
+def split_string(s_str,count=10):
+    per_c=math.ceil(len(s_str)/count)
+    c_cout=0
+    out_str=''
+    split_list=[]
+    for s in s_str:
+        out_str+=s
+        c_cout+=1
+        if c_cout == per_c:
+            split_list.append(out_str)
+            out_str=''
+            c_cout=0
+    if c_cout!=0:
+        split_list.append(out_str)
+    return split_list
+
+
+def frame_extraction(video):
+    if not os.path.exists("./tmp"):
+        os.makedirs("./tmp")
+    temp_folder="./tmp"
+    print("[INFO] tmp directory is being created.")
+
+    vidcap = cv2.VideoCapture(video)
+    count = 0
+
+    while True:
+        success, image = vidcap.read()
+        if not success:
+            break
+        cv2.imwrite(os.path.join(temp_folder, "{:d}.png".format(count)), image)
+        count += 1
+
+        
+def encode_string(input_string,root="./tmp/"):
+    split_string_list=split_string(input_string)
+    for i in range(0,len(split_string_list)):
+        f_name="{}{}.png".format(root,i)
+        secret_enc=lsb.hide(f_name,split_string_list[i])
+        secret_enc.save(f_name)
+        print("[INFO] frame {} holds {}".format(f_name,split_string_list[i]))
+    clear()
+
+
+
+def clean_tmp(path="./tmp"):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        print("[INFO] tmp files have been cleaned up.")
+
+
+def decode_string(video):
+    frame_extraction(video)
+    secret=[]
+    root="./tmp/"
+    for i in range(len(os.listdir(root))):
+        f_name="{}{}.png".format(root,i)
+        secret_dec=lsb.reveal(f_name)
+        if secret_dec == None:
+            break
+        secret.append(secret_dec)
+        
+    result = ''.join([i for i in secret])
+    clear()
+    clean_tmp()
+    print(f'\n[LOG] Encoded data is: "{result}"')
 
 #------------End of My functions------------#
 
