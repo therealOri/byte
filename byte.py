fa638ccdb7d8a351874cfc32617721a3bb2644c1
Ori <45724082+therealOri@users.noreply.github.com>
2022-08-10 12:05:58 -0600

    Update byte.py

--- byte.py
+++ byte.py
@@ -11,84 +11,129 @@ from subprocess import call, STDOUT
 
 
 bl.clear()
-try:
-    option = int(input(f"{bl.banner()}\n\nWhat would you like to do?\n\n1. Encode\n2. Decode\n3. Get hash\n4. Compare hashes\n\nEnter: "))
-except Exception as e:
-    bl.clear()
-    print(f'Value given is not an integer.\nError: {e}')
-    quit()
+while True:
+    try:
+        option = int(input(f"{bl.banner()}\n\nWhat would you like to do?\n\n1. Encode\n2. Decode\n3. Get hash\n4. Compare hashes\n5. Quit?\n\nEnter: "))
+    except Exception as e:
+        bl.clear()
+        print(f"Oops and error occured..Not an integer.\nError: {e}\n\n")
+        input("Press enter to continue...")
+        bl.clear()
 
-if option == 1:
-    bl.clear()
-    vid_or_img = input('What would you like to do?\n\n1. Image  |  (.png, .jpg, etc.)\n2. Video  |  (.webm, .mov, etc.)\n\nEnter: ')
-    vid_or_img = int(vid_or_img)
-    bl.clear()
-    if vid_or_img == 1:
-        bl.encode()
+    if option == 1:
+        bl.clear()
+        while True:
+            try:
+                vid_or_img = int(input('What would you like to Encode?\n\n1. Image  |  (.png, .jpg, etc.)\n2. Video  |  (.webm, .mov, etc.)\n3. Back?\n\nEnter: '))
+            except Exception as e:
+                bl.clear()
+                print(f"Oops and error occured..Not an integer.\nError: {e}\n\n")
+                input("Press enter to continue...")
+                bl.clear()
+                continue
 
-    if vid_or_img == 2:        
-        f_name = input('Please provide the file you would like to inject data into: ')
-        if file_exists(f_name):
             bl.clear()
-            input_string = input('Enter data to be injected: ')
+            if vid_or_img == 1:
+                bl.encode()
+
+            if vid_or_img == 2:        
+                f_name = input('Please provide the file you would like to inject data into: ').replace('\\', ' ').strip()
+                if file_exists(f_name):
+                    bl.clear()
+                    input_string = input('Enter data to be injected: ')
+                    bl.clear()
+                    ofile_name = input('Name of newly encoded file (with extension): ')
+                    bl.clear()
+                else:
+                    print(f'The file with the name "{file}" does not exist in the current directory.')
+                    quit()
+
+                bl.frame_extraction(f_name)
+                call(["ffmpeg", "-i", f_name, "-q:a", "0", "-map", "a", ".tmp/audio.mp3", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+
+                bl.encode_string(input_string)
+                print("Encoding and recompiling data...")
+                call(["ffmpeg", "-i", ".tmp/%d.png" , "-vcodec", "png", ".tmp/video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+                if file_exists('.tmp/audio.mp3'):
+                    call(["ffmpeg", "-i", ".tmp/video.mov", "-i", ".tmp/audio.mp3", "-codec", "copy", "video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+                else:
+                    call(["ffmpeg", "-i", ".tmp/video.mov", "-codec", "copy", "video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+
+                cwd = os.getcwd()
+                os.walk(".tmp/video.mov", cwd)
+                os.rename('video.mov', ofile_name)
+                bl.clear()
+                print("Cleaning up...")
+                bl.clean_tmp()
+                print(f'\n[LOG] Data successfully injected into "{ofile_name}"\n')
+                input("Press enter to continue...")
+                bl.clear()
+
+
+            elif vid_or_img == 3:
+                bl.clear()
+                break
+
+            elif vid_or_img < 1 or vid_or_img > 3:
+                bl.clear()
+                print("Incorrect value given. Please choose a valid option.")
+                input("Press enter to continue...")
+                bl.clear()
+
+
+    elif option == 2:
+        bl.clear()
+        while True:
+            try:
+                dec = int(input('What would you like to decode?\n\n1. Image  |  (.png, .jpg, etc.)\n2. Video  |  (.mp4, .webm, .mov, etc.)\n3. Back?\n\nEnter: '))
+            except Exception as e:
+                bl.clear()
+                print(f"Oops and error occured..Not an integer.\nError: {e}\n\n")
+                input("Press enter to continue...")
+                bl.clear()
+                continue
+
             bl.clear()
-            ofile_name = input('Name of newly encoded file (with extension): ')
-            bl.clear()
-        else:
-            print(f'The file with the name "{file}" does not exist in the current directory.')
-            quit()
+            if dec == 1:
+                print(f"Decoded Data:  {bl.decode()}\n")
+                input("Press enter to continue...")
+                bl.clear()
 
-        bl.frame_extraction(f_name)
-        call(["ffmpeg", "-i", f_name, "-q:a", "0", "-map", "a", ".tmp/audio.mp3", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+            if dec == 2:
+                bl.clear()
+                video = input("Enter the name of video. (With extension): ").replace('\\', ' ').strip().replace('"','').replace("'","")
+                bl.clear()
+                bl.decode_string(video)
 
-        bl.encode_string(input_string)
-        call(["ffmpeg", "-i", ".tmp/%d.png" , "-vcodec", "png", ".tmp/video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+            if dec == 3:
+                bl.clear()
+                break
 
-        if file_exists('.tmp/audio.mp3'):
-            call(["ffmpeg", "-i", ".tmp/video.mov", "-i", ".tmp/audio.mp3", "-codec", "copy", "video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
-        else:
-            call(["ffmpeg", "-i", ".tmp/video.mov", "-codec", "copy", "video.mov", "-y"], stdout=open(os.devnull, "w"), stderr=STDOUT)
+            elif dec < 1 or dec > 3:
+                bl.clear()
+                print("Incorrect value given. Please choose a valid option.")
+                input("Press enter to continue...")
+                bl.clear()
+                
 
-        cwd = os.getcwd()
-        os.walk(".tmp/video.mov", cwd)
-        os.rename('video.mov', ofile_name)
-        bl.clean_tmp()
-        print(f'\n[LOG] Data successfully injected into "{ofile_name}"')
 
-    elif vid_or_img == 0 or vid_or_img > 2:
+
+    elif option == 3:
+        bl.check()
+
+    elif option == 4:
         bl.clear()
-        print("Incorrect value given. Please choose a valid option.")
-        quit()
-
-
-elif option == 2:
-    bl.clear()
-    dec = input('What would you like to decode?\n\n1. Image  |  (.png, .jpg, etc.)\n2. Video  |  (.webm, .mov, etc.)\n\nEnter: ')
-    dec = int(dec)
-    bl.clear()
-    if dec == 1:
-        print(f"Decoded Data:  {bl.decode()}")
-
-    if dec == 2:
+        fhash = input('Please provide a valid blake2b hash to compare: ')
         bl.clear()
-        video = input("Enter the name of video. (With extension): ")
+        bl.compare(fhash)
+
+    elif option == 5:
         bl.clear()
-        bl.decode_string(video)
+        exit('Goodbye!')
 
-
-    elif dec == 0 or dec > 2:
+    elif option < 1 or option > 5:
         bl.clear()
-        print("Incorrect value given. Please choose a valid option.")
-        quit()
-
-elif option == 3:
-    bl.check()
-elif option == 4:
-    bl.clear()
-    fhash = input('Please provide a valid blake2b hash to compare: ')
-    bl.clear()
-    bl.compare(fhash)
-else:
-    bl.clear()
-    print("Incorrect value given. Please choose a valid option.")
-    quit()
+        print("Unknown option...")
+        input("Press enter to continue...")
+        bl.clear()
+        continue
